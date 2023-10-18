a9b7fb3a84ed380f3786ba662d826c14389f431a
mp143mani <127532509+mp143mani@users.noreply.github.com>
2023-10-18 12:59:01 +0530

    s

--- src/Pizzawindow.js
+++ src/Pizzawindow.js
@@ -1,17 +1,16 @@
-import React, {   useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Navbar from 'react-bootstrap/Navbar';
-import env from './enviroinment'
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import './CSS/Pizzawindow.css';
-import tomatopizza from './assets/wp6905765-dominos-pizza-wallpapers.jpg';
-import onionpizza from './assets/wp6905795-dominos-pizza-wallpapers.jpg';
-import paneerpizza from './assets/wp6905748-dominos-pizza-wallpapers.jpg';
-import cheesepizza from './assets/wp6905725-dominos-pizza-wallpapers.jpg';
+import Navbar from "react-bootstrap/Navbar";
+// import env from "./Backendurl";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import "./CSS/Pizzawindow.css";
+import thakkali from "../src/assets/veg/thakkali.jfif";
+import kathari from "../src/assets/veg/kathari.jpg";
+import beens from "../src/assets/veg/beens.jpg";
+import caret from "../src/assets/veg/images1.jfif";
 import Button from "react-bootstrap/Button";
-
 
 export default function Pizzawindow() {
   const navigate = useNavigate();
@@ -20,51 +19,45 @@ export default function Pizzawindow() {
     navigate("/payment");
   };
   const totalPrice = 5;
-  const Array =[
-                 {img:onionpizza,
-                    title:"Onion pizaa",
-                    cost: "$5"},
-                 {img:cheesepizza,
-                    title:"Cheese pizaa",
-                    cost: "$5"},
-                 {img:tomatopizza,
-                    title:"Tomato pizaa",
-                    cost: "$5"},
-                 {img:paneerpizza,
-                    title:"Paneer pizaa",
-                     cost: "$5"}
-                    ]
+  const Array = [
+    { img: thakkali, title: "Tomato", cost: "RS:15" },
+    { img: caret, title: " Carrot", cost: "RS:8" },
+    { img: beens, title: "Beans", cost: "RS:13" },
+    { img: kathari, title: "Eggplant", cost: "RS:40" },
+  ];
   return (
-    < >
-    <div >
-      {['xl'].map((expand) => (
-        <Navbar key={expand} bg="dark" expand={expand} className=""> 
-            <Navbar.Brand href="#" ><span id="color">Pizza Mania</span></Navbar.Brand>
-        </Navbar>
-      ))}
- </div>
-     <div className="set " >
-     <div className="set1 " >
-      <Row xs={1} md={2} className="g-4 " >
-      {Array.map((element) => (
-        <Col >
-          <Card >
-            <Card.Img variant="top" style={{ height:"30rem"}} src={element.img} />
-            <Card.Body>
-              <section>{element.title}</section>
-              <Card.Text>
-                {element.cost}
-              </Card.Text>
-              <Button onClick={()=>Move(totalPrice)}>Buy</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
-    
-     </div>
-     </div>
+    <>
+      <div>
+        {["xl"].map((expand) => (
+          <Navbar key={expand} bg="dark" expand={expand} className="">
+            <Navbar.Brand href="#">
+              <span id="color">Pizza Mania</span>
+            </Navbar.Brand>
+          </Navbar>
+        ))}
+      </div>
+      <div className="set ">
+        <div className="set1 ">
+          <Row xs={1} md={2} className="g-4 ">
+            {Array.map((element) => (
+              <Col>
+                <Card>
+                  <Card.Img
+                    variant="top"
+                    style={{ height: "30rem" }}
+                    src={element.img}
+                  />
+                  <Card.Body>
+                    <section>{element.title}</section>
+                    <Card.Text>{element.cost}</Card.Text>
+                    <Button onClick={() => Move(totalPrice)}>Buy</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </div>
+      </div>
     </>
-
   );
 }
