c827520ecc4073836dd884c343b7f89b0f33898a
mp143mani <127532509+mp143mani@users.noreply.github.com>
2023-10-19 07:46:31 +0530

    si

--- src/Backendurl.js
+++ src/Backendurl.js
@@ -1,11 +1,4 @@
 const env = {
     apiurl: "https://backendp-n8dl.onrender.com"
 }
-
 export default env;
-
-// const env = {
-//     apiurl: "http://localhost:3000"
-// }
-
-// export default env;
